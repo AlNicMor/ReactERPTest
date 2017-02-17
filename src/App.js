cf18e6aba13b200fc9448c3d12c8ea2fdcfe0d5d
amoreno <alan.moreno@quadminds.com>
2017-02-17 18:58:35 -0300

    Just some changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component, PropTypes } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import { List, ListItem } from 'material-ui/List';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import AutoComplete from 'material-ui/AutoComplete';
+import Checkbox from 'material-ui/Checkbox';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
 import { css } from 'aphrodite';
 import { styles } from './AppStyles.js';
 
@@ -10,6 +16,12 @@ import { styles } from './AppStyles.js';
 
 
 class App extends Component {
+
+  state = {
+    dataSource: ['Alan', 'Guada', 'Ana'],
+  };
+
+
   render() {
     return (
       <div className={css(styles.container)}>
@@ -19,19 +31,29 @@ class App extends Component {
             <AppBar
               title="Title"
               iconElementLeft={<span></span>}
+              iconElementRight={<FlatButton
+                backgroundColor="#a4c639"
+                hoverColor="#8AA62F"
+                label="New" />}
+            />
+            <AutoComplete
+              dataSource={this.state.dataSource}
+              filter={AutoComplete.caseInsensitiveFilter}
             />
             <List>
               <ListItem
                 primaryText="Brendan Lim"
                 nestedItems={[
                   <ListItem
-                  key={1}
-                  primaryText="Starred"
-                />,
-                <ListItem
-                  key={2}
-                  primaryText="Another Starred"
-                />,
+                    key={1}
+                    primaryText="Starred"
+                    rightIconButton={<RaisedButton label="Edit" secondary={true} />}
+                  />,
+                  <ListItem
+                    key={2}
+                    primaryText="Another Starred"
+                    rightIconButton={<RaisedButton label="Edit" secondary={true} />}
+                  />,
                 ]}
               />
               <ListItem
